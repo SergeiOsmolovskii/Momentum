de846b839b60f75ac737f457c4414a35894023e1
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2021-10-18 23:02:40 +0300

    feat: add sound-control and progress-bar for audio

--- js/audio.js
+++ js/audio.js
@@ -2,10 +2,15 @@
 
 import playList from './playList.js';
 
+const progress = document.querySelectorAll('.control-input');
 const playListBlock = document.querySelector('.play-list');
 const playPrevBtn = document.querySelector('.play-prev');
 const playNextBtn = document.querySelector('.play-next');
 const playBtn = document.querySelector('.play');
+const soundButton = document.querySelector('.sound-button');
+const soundControl = document.querySelector('.sound-bar-progress');
+const audioLengthControl = document.querySelector('.audio-length-control');
+
 const audio = new Audio();
 let isPlay = false;
 let currentAudio = 0;
@@ -18,6 +23,11 @@ playList.forEach(item => {
     li.textContent = item.title;
     playListBlock.append(li); 
 })
+
+progress.forEach(item => item.addEventListener('input', function () {
+    const value = this.value;
+    this.style.background = `linear-gradient(to right, #c5b358 0%, #c5b358 ${value}%, #C4C4C4 ${value}%, #C4C4C4)`;
+}))
 
 let playListLi = document.querySelectorAll('.play-item');
 
@@ -37,7 +47,7 @@ export function playAudio() {
 
 function toggleBtn() {
     playBtn.classList.toggle('pause');
-  }
+}
 
 function playCurrentAudio() {
     audio.src = playList[currentAudio].src;
@@ -65,7 +75,55 @@ function playNext() {
     playCurrentAudio();
 }
 
+function changeVolume() {
+    audio.volume = soundControl.value / 100;
+    if (audio.volume === 0) soundButton.classList.toggle('sound-mute');
+    else soundButton.classList.remove('sound-mute');
+}
+
+function mute() {
+    soundButton.classList.toggle('sound-mute');
+    if (soundButton.classList.contains('sound-mute')) {
+        audio.volume = 0;
+        soundControl.value = 0;
+        soundControl.style.background = `linear-gradient(to right, #c5b358 0%, #c5b358 0%, #C4C4C4 0%, #C4C4C4)`;
+    } else {
+        audio.volume = 0.5;
+        soundControl.value = 50;
+        soundControl.style.background = `linear-gradient(to right, #c5b358 0%, #c5b358 50%, #C4C4C4 50%, #C4C4C4)`;
+    }
+}
+
+function audioPprogress() {
+    const percent = Math.floor((audio.currentTime / audio.duration) * 100);
+    audioLengthControl.value = percent;
+    audioLengthControl.style.background = `linear-gradient(to right, #c5b358 0%, #c5b358 ${percent}%, #C4C4C4 ${percent}%, #C4C4C4)`;
+    if (audio.ended) {
+        audio.currentTime = 0;
+        togglePlayClases();
+    }
+}
+
+function changeProgressBar(e) {
+    const time = (e.offsetX / audioLengthControl.offsetWidth) * audio.duration;
+    audio.currentTime = time;
+    if (audio.ended) {
+        audio.currentTime = 0;
+        togglePlayClases();
+    }
+}
+
 playBtn.addEventListener('click', playAudio);
 playBtn.addEventListener('click', toggleBtn);
 playPrevBtn.addEventListener('click', playPrev);
 playNextBtn.addEventListener('click', playNext);
+
+soundButton.addEventListener('click', mute);
+soundControl.addEventListener('change', changeVolume);
+audioLengthControl.addEventListener('click', changeProgressBar);
+
+let mousedown = false;
+audio.addEventListener('timeupdate', audioPprogress);
+audioLengthControl.addEventListener('mousemove', (e) => mousedown && changeProgressBar(e));
+audioLengthControl.addEventListener('mousedown', () => mousedown = true);
+audioLengthControl.addEventListener('mouseup', () => mousedown = false);
